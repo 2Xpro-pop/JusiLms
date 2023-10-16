783795abec91d4fb28fbbaabd924d8fb7b9a828e
2Xpro-pop <bay.asanov@mail.ru>
2023-10-16 15:04:34 +0600

    HomeWorks Added

---
+++ JusiLms/Controllers/HomeWorksController.cs
@@ -0,0 +1,76 @@
+﻿using JusiLms.Models;
+using JusiLms.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JusiLms.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HomeWorksController : ControllerBase
+{
+    private readonly IHomeWorksService _homeWorksService;
+
+    public HomeWorksController(IHomeWorksService homeWorksService)
+    {
+        _homeWorksService = homeWorksService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllHomeWorks()
+    {
+        var homeWorks = await _homeWorksService.GetAllHomeWorks();
+        return Ok(homeWorks);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(HomeWork work)
+    {
+        await _homeWorksService.Create(work);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update(HomeWork work)
+    {
+        await _homeWorksService.Update(work);
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(HomeWork work)
+    {
+        await _homeWorksService.Delete(work);
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(Guid id)
+    {
+        var homeWork = await _homeWorksService.Get(id);
+        if (homeWork == null)
+            return NotFound();
+
+        return Ok(homeWork);
+    }
+
+    [HttpGet("user/{userId}")]
+    public async Task<IActionResult> GetUserHomeworks(Guid userId)
+    {
+        var homeWorks = await _homeWorksService.GetUserHomeworks(userId);
+        return Ok(homeWorks);
+    }
+
+    [HttpPost("{userId}/{homeWorkId}")]
+    public async Task<IActionResult> AddHomeworkToUser(Guid userId, Guid homeWorkId)
+    {
+        await _homeWorksService.AddHomeworkToUser(userId, homeWorkId);
+        return Ok();
+    }
+
+    [HttpDelete("{userId}/{homeWorkId}")]
+    public async Task<IActionResult> RemoveHomeworkToUser(Guid userId, Guid homeWorkId)
+    {
+        await _homeWorksService.RemoveHomeworkToUser(userId, homeWorkId);
+        return Ok();
+    }
+}
---
+++ JusiLms/Services/Api/IHomeWorksApi.cs
@@ -0,0 +1,31 @@
+﻿using JusiLms.Models;
+using Refit;
+
+namespace JusiLms.Services.Api;
+
+public interface IHomeWorksApi
+{
+    [Get("/api/HomeWorks")]
+    Task<IEnumerable<HomeWork>> GetAllHomeWorks();
+
+    [Post("/api/HomeWorks")]
+    Task Create([Body] HomeWork work);
+
+    [Put("/api/HomeWorks")]
+    Task Update([Body] HomeWork work);
+
+    [Delete("/api/HomeWorks")]
+    Task Delete([Body] HomeWork work);
+
+    [Get("/api/HomeWorks/{id}")]
+    Task<HomeWork> Get(Guid id);
+
+    [Get("/api/HomeWorks/user/{userId}")]
+    Task<IEnumerable<HomeWork>> GetUserHomeworks(Guid userId);
+
+    [Post("/api/HomeWorks/{userId}/{homeWorkId}")]
+    Task AddHomeworkToUser(Guid userId, Guid homeWorkId);
+
+    [Delete("/api/HomeWorks/{userId}/{homeWorkId}")]
+    Task RemoveHomeworkToUser(Guid userId, Guid homeWorkId);
+}
