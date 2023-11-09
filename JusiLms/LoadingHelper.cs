02ee8273106617f11e067aa743ffaca5f526380d
2Xpro-pop <bay.asanov@mail.ru>
2023-11-09 12:13:31 +0600

    using loading

--- JusiLms/LoadingHelper.cs
+++ JusiLms/LoadingHelper.cs
@@ -1,7 +1,11 @@
-﻿namespace JusiLms;
+﻿using System.Runtime.CompilerServices;
+using MudBlazor;
+
+namespace JusiLms;
 
 public static class LoadingHelper
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static async Task Load(Action<bool> setLoading, Func<Task> task, Action stateHasChanged)
     {
         setLoading(true);
@@ -10,4 +14,35 @@ public static class LoadingHelper
         setLoading(false);
         stateHasChanged();
     }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static async Task LoadAndCatch(Action<bool> setLoading, Func<Task> task, Action stateHasChanged, ILogger logger, ISnackbar snackbar, string messageForLogger, string messageForSnackbar)
+    {
+        try
+        {
+            setLoading(true);
+            stateHasChanged();
+            await task();
+            setLoading(false);
+            stateHasChanged();
+        }
+        catch(Refit.ValidationApiException exc)
+        {
+            await logger.LogException(exc, messageForLogger);
+            snackbar.Add(messageForSnackbar, Severity.Error);
+        }
+        catch(Refit.ApiException exc)
+        {
+            await logger.LogException(exc, messageForLogger);
+            snackbar.Add(messageForSnackbar, Severity.Error);
+        }
+        catch(Exception exc)
+        {
+#pragma warning disable CA2254
+            logger.LogError(exc, messageForLogger);
+#pragma warning restore CA2254
+            snackbar.Add(messageForSnackbar, Severity.Error);
+        }
+    }
+
+
 }
